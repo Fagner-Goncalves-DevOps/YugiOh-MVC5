209103def5108e743848c22e5cc7f986f4371ec0
FagnerGoncalves <fagner@ansata.com.br>
2022-09-22 22:48:44 -0300

    yugioh Inicio

---
+++ src/AspNet5Mvc-YuGiOh/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNet5Mvc_YuGiOh.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
