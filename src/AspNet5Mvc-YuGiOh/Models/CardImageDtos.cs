afb45e793d3230e18b98a2494da24a0668243d56
FagnerGoncalves <fagner@ansata.com.br>
2022-10-03 23:55:08 -0300

    Criação Modelos Dados

---
+++ src/AspNet5Mvc-YuGiOh/Models/CardImageDtos.cs
@@ -0,0 +1,13 @@
+﻿namespace AspNet5Mvc_YuGiOh.Models
+{
+    public class CardImageDtos
+    {
+        public int id { get; set; }
+        public string image_url { get; set; }
+        public string image_url_small { get; set; }
+    }
+
+
+
+
+}
---
+++ src/AspNet5Mvc-YuGiOh/Models/CardSetDtos.cs
@@ -0,0 +1,15 @@
+﻿namespace AspNet5Mvc_YuGiOh.Models
+{
+    public class CardSetDtos
+    {
+        public string set_name { get; set; }
+        public string set_code { get; set; }
+        public string set_rarity { get; set; }
+        public string set_rarity_code { get; set; }
+        public string set_price { get; set; }
+    }
+
+
+
+
+}
