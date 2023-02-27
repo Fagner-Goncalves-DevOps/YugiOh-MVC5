2501a3ed3bd97a22a27c83c6dbbefd40bb0d24d4
Fagner <fagnergoncalves15@gmail.com>
2023-02-27 17:57:16 -0300

    Alterando arquitetura

--- src/AspNet5Mvc-YuGiOh/Controllers/DataController.cs
+++ src/AspNet5Mvc-YuGiOh/Controllers/DataController.cs
@@ -14,27 +14,24 @@ namespace AspNet5Mvc_YuGiOh.Controllers
     public class DataController : Controller
     {
 
-        //APIS PARA TESTE 
         //private readonly string ApiUrl = "https://db.ygoprodeck.com/api/v7/cardinfo.php";
         //private readonly string ApiUrl = "https://db.ygoprodeck.com/api/v7/cardinfo.php?name=Tornado%20Dragon";
-        private readonly string ApiUrl = "https://db.ygoprodeck.com/api/v7/cardinfo.php?type=Normal%20Monster";
+        //private readonly string ApiUrl = "https://db.ygoprodeck.com/api/v7/cardinfo.php?type=Normal%20Monster";
 
 
         public async Task<IActionResult> Index()
         {
-            List<DataDtos> ListaTotal = new List<DataDtos>();
+            //List<DataDtos> ListaTotal = new List<DataDtos>();
 
-            using (var httpClient = new HttpClient())
-            {
-                using (var response = await httpClient.GetAsync(ApiUrl))
-                {
-                    var ApiResponse = await response.Content.ReadAsStringAsync();
-                    ListaTotal = JsonSerializer.Deserialize<Root>(ApiResponse).data; //não precisa passar um listagem, EF ja fez isso
-
-                    //var dados = (from a in ListaTotal select new { a.id, a.name, a.type}).ToList();
-                }
-            }
-            return View(ListaTotal.Take(10)); //pegando 10 apenas para testar
+            //using (var httpClient = new HttpClient())
+            //{
+            //    using (var response = await httpClient.GetAsync(ApiUrl))
+            //    {
+            //        var ApiResponse = await response.Content.ReadAsStringAsync();
+            //        ListaTotal = JsonSerializer.Deserialize<Root>(ApiResponse).data; //não precisa passar um listagem, EF ja fez isso
+            //    }
+            //}
+            //return View(ListaTotal.Take(10)); //pegando 10 apenas para testar
         }
     }
 }
---
+++ src/AspNet5Mvc-YuGiOh/Interfaces/ICardDatabase.cs
@@ -0,0 +1,11 @@
+﻿using AspNet5Mvc_YuGiOh.Models;
+using System.Threading.Tasks;
+
+namespace AspNet5Mvc_YuGiOh.Interfaces
+{
+    public interface ICardDatabase
+    {
+        Task<DataDtos> GetAllCads();
+        Task<Root> GetCardsByType(DataDtos dataDtos);
+    }
+}
